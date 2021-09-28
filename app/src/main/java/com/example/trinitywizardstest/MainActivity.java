62326bf886d72426a206ece1a0beabcf4e76bd2b
jsboon <boonjinsheng01@gmail.com>
2021-09-28 14:40:19 +0800

    1

--- app/src/main/java/com/example/trinitywizardstest/MainActivity.java
+++ app/src/main/java/com/example/trinitywizardstest/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.trinitywizardstest;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        login = findViewById(R.id.login_btn);
+//        login.setOnClickListener(view -> startActivity(new Intent(MainActivity.this, Login.class)));
+
     }
 }
